31d0f46e6bcf69d74113bc3a68da1eb2d7e08665
RatonRoy <royraton18@gmail.com>
2022-07-01 04:38:43 +0600

    successfully connected mongodb database with server side

--- index.js
+++ index.js
@@ -8,10 +8,23 @@ const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
+// username : powerHack
+// password : ywpGEKkI0MIBl5VS
 
 // use middleware 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = "mongodb+srv://powerHack:ywpGEKkI0MIBl5VS@programming-hero.cx72s.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+	console.log('db connected ');
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
 	res.send(' Start programing hero projects');
