a0fe47257074074c16f0a490bbb7551b96b68f59
RatonRoy <royraton18@gmail.com>
2022-07-01 04:08:00 +0600

    set up all dependencies

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectID } = require('bson');
+const ObjectId = require('mongodb').ObjectId;
+
 const app = express();
-const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+// use middleware 
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send(' Start programing hero projects');
 })
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`listening on port ${port}`);
 })
