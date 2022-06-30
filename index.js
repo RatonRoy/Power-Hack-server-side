dc08dece409109976a74fb892f8d3cc920f41067
RatonRoy <royraton18@gmail.com>
2022-07-01 05:37:42 +0600

    add data to the mongodb server

--- index.js
+++ index.js
@@ -18,13 +18,23 @@ app.use(express.json());
 
 const uri = "mongodb+srv://powerHack:ywpGEKkI0MIBl5VS@programming-hero.cx72s.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-	console.log('db connected ');
-  client.close();
-});
+async function run() {
+	try {
+		await client.connect();
+		const userCollection = client.db("All-bills").collection("bill");
+		const bill = {
+			name: "abc", 
+			email: "myname@gmail.com", 
+			mobile: " 9345454"
+		}
+		const result = await userCollection.insertOne(bill);
+		console.log(`A document was inserted with the _id: ${result.insertedId}`);
+	}
+	finally {
 
+	}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
 	res.send(' Start programing hero projects');
